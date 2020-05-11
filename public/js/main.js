275ba4ffa94f57449e66f9125824e37eaa87eeab
Gaute <gaute@rauboti.net>
2020-05-12 00:34:05 +0200

    Pictures for the Service page

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,29 @@ var pictureBank = []
 var picturePosition = 0
 
 $(document).ready(function() {
-    if ($('#pictures').length) {
-        var arr = $('#pictures').val().split(',');
-        console.log(arr);
+    if ($('#pictureArray').length) {
+        pictureBank = $('#pictureArray').val().split(',');
+        console.log(pictureBank);
+        if (pictureBank.length > 1) {
+            $('.arrowRow').css({'height': '4vh', 'margin-bottom': '1vh'});
+            $('.pagePic').css({'height': '70vh', 'maxHeight': '70vh'});
+            $('.pagePic img').css({'maxHeight': '70vh'});
+
+            $('.arrow').click(function() {
+                if ($(this).attr('id') === 'arrowRight') {
+                    picturePosition++;
+                    if (picturePosition == pictureBank.length) {
+                        picturePosition = 0;
+                    }
+                } else if ($(this).attr('id') === 'arrowLeft') {
+                    picturePosition--;
+                    if (picturePosition < 0) {
+                        picturePosition = pictureBank.length-1;
+                    }
+                }
+                changePicture(picturePosition);
+            })
+        }
     }
 
     $('.language').click(function() {
@@ -23,3 +43,9 @@ $(document).ready(function() {
         window.location.href = url;
     });
 });
+
+function changePicture(newPos) {
+    console.log(newPos + ' - ' +  pictureBank[newPos])
+    var newSrc = '/img/upload/service/' + pictureBank[newPos];
+    $('#picture').attr('src', newSrc);
+}
